66ad5ecb9d41ba48f0d648cd45076804df1c6106
tpauchard <t.pauchard@hokoha.com>
2024-04-26 11:59:02 +0200

    Using ViewSiwtcher Issue

--- DatatTemplatePerfIssue/NewPage1.xaml.cs
+++ DatatTemplatePerfIssue/NewPage1.xaml.cs
@@ -8,6 +8,20 @@ public partial class NewPage1 : ContentPage
     public NewPage1()
     {
         InitializeComponent();
-        delay.LoadView();
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        switcher.SelectedIndex = 0;
+    }
+
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+        switcher.SelectedIndex = 1;
+    }
+
+    private void Button_Clicked_2(object sender, EventArgs e)
+    {
+        switcher.SelectedIndex = 2;
     }
 }
