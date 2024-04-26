2e2357bbe5b71de3ba10f29f12d5da1debb41822
tpauchard <t.pauchard@hokoha.com>
2024-04-26 11:38:13 +0200

    Trying with Sharpnado.Tabs.Maui DelayedView

--- DatatTemplatePerfIssue/NewPage1.xaml.cs
+++ DatatTemplatePerfIssue/NewPage1.xaml.cs
@@ -8,29 +8,6 @@ public partial class NewPage1 : ContentPage
     public NewPage1()
     {
         InitializeComponent();
-    }
-
-    private void Button_Clicked(object sender, EventArgs e)
-    {
-        ObservableCollection<Col> cols = new ObservableCollection<Col>();
-        for (int i = 0; i < 8; i++)
-        {
-            cols.Add(new Col("Title" + i));
-        }
-        ObservableCollection<Row> rows = new ObservableCollection<Row>();
-        for (int i = 0; i < 5; i++)
-        {
-            rows.Add(new Row("Row " + i, "Description " + i, cols));
-        }
-
-        var timer = new Stopwatch();
-        timer.Start();
-        BindableLayout.SetItemsSource(Rows, rows);
-        timer.Stop();
-
-        TimeSpan timeTaken = timer.Elapsed;
-        string foo = "Time taken: " + timeTaken.ToString(@"m\:ss\.fff");
-        Console.Out.WriteLine(foo);
-        lbLoaded.Text = foo;
+        delay.LoadView();
     }
 }
